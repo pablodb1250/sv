010b5ac20e8895b7b72447fd313db490f06d83e1
pablodb1250 <pablodb1250@gmail.com>
2016-01-16 16:54:21 -0300

    InstBower ejercio 1
    Instalacion de bower, creacion de archivos de configuracion y
    modificacion de app.js

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
+'use strict';
+
+// Loading dependencies
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+// Initializing express application
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// Body Parser
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Logger
+var logger = require('morgan');
+app.use(logger('dev'));
+
+//Cookies / Session
+var cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+// Layout SetUp
+var exphbs = require('express-handlebars');
+
+// Stylus SetUp
+var stylus = require('stylus');
+var nib = require('nib');
+
+// Handlebars SetUp
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials'
+}));
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+var routes = require('./routes/index');
+var users = require('./routes/users');
 app.use('/', routes);
 app.use('/users', users);
 
@@ -33,9 +54,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +64,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -55,6 +71,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
